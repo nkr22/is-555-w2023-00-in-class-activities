b57b0dab15e73f2180aeeb2176fb5e3b453f5f2c
nkr22 <81866898+nkr22@users.noreply.github.com>
2023-02-03 20:15:35 -0700

    checking for typos

--- 5 pivots/section 1.R
+++ 5 pivots/section 1.R
@@ -9,10 +9,10 @@
 # 
 # pivot_longer(
 #   data,                             <-- The incoming tibble, usually comes in via a pipe
-#   cols,                             <-- This is where you say which columns you're pulling down to long format
+#   **cols,                             <-- This is where you say which columns you're pulling down to long format
 #                                           (any that you don't name here are assumed to be "id columns" that 
 #                                           will repeat with eachof the variables being converted to long format)
-#   names_to = "name",                <-- This is the column name that will "house" the column names that you have 
+#   **names_to = "name",                <-- This is the column name that will "house" the column names that you have 
 #                                           specified in the `cols` parameter. Often you can just leave this as the 
 #                                           default and it will name it "name", but you can also be more descriptive
 #   names_prefix = NULL,              <-- You can (optionally) indicate a text-based prefix to auto-remove as the
@@ -23,7 +23,7 @@
 #   names_ptypes = NULL,              <-- beyond our scope - explore this on your own if you want.
 #   names_transform = NULL,           <-- beyond our scope - explore this on your own if you want.
 #   names_repair = "check_unique",    <-- beyond our scope - explore this on your own if you want.
-#   values_to = "value",              <-- this is the column that will "house" the values corresponding to each of 
+#   **values_to = "value",              <-- this is the column that will "house" the values corresponding to each of 
 #                                           the column name labels in the `names_to` column that you specified above.
 #   values_drop_na = FALSE,           <-- wider data is often "sparse", meaning that it has lots of nulls when not all
 #                                           rows have a value for each wide column. It's usually efficient to filter 
@@ -39,7 +39,7 @@
 #                                           columns, but you can usually ignore this parameter because you will
 #                                           be explicitly specifying the `names_from` and `values_from` parameters
 #   id_expand = FALSE,                <-- beyond our scope - explore this on your own if you want.
-#   names_from = name,                <-- Here you specify the column that contains the value names or "labels"
+#   **names_from = name,                <-- Here you specify the column that contains the value names or "labels"
 #                                           (it defaults to `name` because that's the default from `pivot_longer()`)
 #   names_prefix = "",                <-- beyond our scope - explore this on your own if you want.
 #   names_sep = "_",                  <-- beyond our scope - explore this on your own if you want.
@@ -48,7 +48,7 @@
 #   names_vary = "fastest",           <-- beyond our scope - explore this on your own if you want.
 #   names_expand = FALSE,             <-- beyond our scope - explore this on your own if you want.
 #   names_repair = "check_unique",    <-- beyond our scope - explore this on your own if you want.
-#   values_from = value,              <-- Here you specify the column that contains the values corresponding to the
+#   **values_from = value,              <-- Here you specify the column that contains the values corresponding to the
 #                                           names specified in the `names_from` parameter.
 #                                           (it defaults to `value` because that's the default from `pivot_longer()`)
 #   values_fill = NULL,               <-- This option allows you to provide the "default value" for any rows in the
@@ -71,32 +71,79 @@ drinks <- tribble(
 )
 
 # Let's make it more tidy. pivot_longer
+drinks %>% 
+  pivot_longer(soda:sparkling_water,
+              names_to='drink_type',
+              values_to='liters_per_capita')
 
 
 # New data:
 gap <- read_csv('https://www.dropbox.com/s/dv1a1ldkuyoftn2/gap_smaller.csv?dl=1')
 
 # Start with just country, year, lifeExp, pivot wider
-
+gap %>% 
+  select(country, year, lifeExp) %>% 
+  pivot_wider(names_from=country, values_from = lifeExp)
 
 # Now let's go the other way: pivot all three measures longer
-
+# to make longer- take measure columns and create value pairs
+gap %>% 
+  pivot_longer(
+    lifeExp:gdpPercap, 
+    names_to = 'measure',
+    values_to='value'
+  )
+  
 
 # Pivot all three measures wider. Gets messy, but 
 # sometimes data comes to you in this format
 
+#as a label for the column, it creates a key-value pair
+gap_wide <- gap %>% 
+  pivot_wider(names_from = year,
+              values_from=lifeExp:gdpPercap)
 
+gap_wide %>% 
+  pivot_longer(lifeExp_1952:gdpPercap_2007,
+               names_to = c('measure', 'year'),
+               names_sep='_')
+
+#be able to extract year from making the wide data long
 
 # New data:
 ri <- read_csv('https://www.dropbox.com/s/dfjgfytyek44u61/rel_inc.csv?dl=1')
 bnames <- read_csv('https://www.dropbox.com/s/6bck5fy4aag76kw/baby_names.csv?dl=1')
 bob <- read_csv('https://www.dropbox.com/s/mozqpceit51hia7/bob_ross.csv?dl=1')
 
+
 # see if you can pivot the religious income data into a tidier format
-ri
 
-# see if you can widen the baby names into a format with one row per name (and sex)
-bnames
+ri %>% 
+  pivot_longer(cols=!religion,
+               names_to='income_bracket',
+               values_to='household_count') %>% 
+  filter(income_bracket == '>150k')
+  
 
-# make the bob ross data tidier
-bob
+#show only the top income BRACKET for each religion
+
+ri %>% 
+  pivot_longer(cols = `<$10k`:`Don't know/refused`,
+               names_to = 'income_brakcet',
+               values_to = 'num_people'
+  ) %>% 
+  pivot_wider(names_from = religion,
+              values_from = num_people)
+
+
+#make bob ross data tidier then
+# episodes that depict some sort of "frame" but not any bridges
+# count the episodes with paintings that contain "MOUNTAIN" that depict objects containing the string "tree"
+
+bob %>% 
+  pivot_longer(apple_frame:wood_framed,
+               names_to='item',
+               values_to='is_present') %>% 
+  filter((str_detect(item, 'fname') & is_present == 1) |
+        (item=='bridge' & is_present == 0)) %>% 
+  distinct(episode)
